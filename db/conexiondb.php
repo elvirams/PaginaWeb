b52a3d75b1d87155b02967d462bdcc3d1c691998
elvirams <114144314+elvirams@users.noreply.github.com>
2023-06-11 13:55:58 +0200

    Update conexiondb.php

--- db/conexiondb.php
+++ db/conexiondb.php
@@ -3,13 +3,15 @@
     class Database {
         public static function connect () 
         {
-            $host='mariadb';
-            $dbname=getenv('MARIADB_DATABASE');
-
+            #$host='mariadb';
+            #$dbname=getenv('MARIADB_DATABASE');
+            #$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "servidormariadbtfg.mariadb.database.azure.com", "adminazure@servidormariadbtfg", {your_password}, {your_database}, 3306);
             try {
-                $dsn = 'mysql:host='.$host.";dbname=".$dbname.";charset=UTF8";
-                $dbh = new PDO($dsn, getenv('MARIADB_USER'), getenv('MARIADB_PASSWORD'));
-                return $dbh;
+                #$dsn = 'mysql:host='.$host.";dbname=".$dbname.";charset=UTF8";
+                #$dbh = new PDO($dsn, getenv('MARIADB_USER'), getenv('MARIADB_PASSWORD'));
+                $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "servidormariadbtfg.mariadb.database.azure.com", "adminazure@servidormariadbtfg", {your_password}, {your_database}, 3306);
+   
+                return $con;
                 
             } catch (PDOException $e){
                 echo $e->getMessage();
