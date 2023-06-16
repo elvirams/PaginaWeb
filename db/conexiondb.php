ab7cfd128a3925a2605131a4078272fecce827e3
elvirams <114144314+elvirams@users.noreply.github.com>
2023-06-16 18:03:04 +0200

    Update conexiondb.php

--- db/conexiondb.php
+++ db/conexiondb.php
@@ -6,7 +6,11 @@ class Database {
 
         try {
             $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname . ';charset=UTF8';
-            $dbh = new PDO($dsn, 'adminazure@servidormariadbtfg', 'emorcillos06@iesalbarregas.es');
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => '/path/to/ca.pem',  // Ruta al archivo de certificado CA
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // Desactivar la verificación del certificado del servidor (solo para pruebas)
+            );
+            $dbh = new PDO($dsn, 'adminazure@servidormariadbtfg', 'emorcillos06@iesalbarregas.es', $options);
             return $dbh;
         } catch (PDOException $e) {
             echo $e->getMessage();
