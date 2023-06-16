04650bb3a8cf368a390ed6140304203f4510b9d3
elvirams <114144314+elvirams@users.noreply.github.com>
2023-06-16 17:50:28 +0200

    Update conexiondb.php

--- db/conexiondb.php
+++ db/conexiondb.php
@@ -1,26 +1,16 @@
 <?php
-    # Método para realizar la conexión a la BBDD. Obtiene los datos de conexión de variables de entorno existentes en el fichero .env.
-    class Database {
-        public static function connect () 
-        {
-            #$host='mariadb';
-            #$dbname=getenv('MARIADB_DATABASE');
-            $con=mysqli_init();
-            mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-            mysqli_real_connect($con, "servidormariadbtfg.mariadb.database.azure.com", "adminazure@servidormariadbtfg", {your_password}, {your_database}, 3306);
-            
-            try {
-                #$dsn = 'mysql:host='.$host.";dbname=".$dbname.";charset=UTF8";
-                #$dbh = new PDO($dsn, getenv('MARIADB_USER'), getenv('MARIADB_PASSWORD'));
-                $con=mysqli_init(); 
-                mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-                mysqli_real_connect($con, "servidormariadbtfg.mariadb.database.azure.com", "adminazure@servidormariadbtfg", {your_password}, {your_database}, 3306);
-   
-                return $con;
-                
-            } catch (PDOException $e){
-                echo $e->getMessage();
-            }
+class Database {
+    public static function connect() {
+        $host = 'servidormariadbtfg.mariadb.database.azure.com';
+        $dbname = 'moviles';
+
+        try {
+            $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname . ';charset=UTF8';
+            $dbh = new PDO($dsn, 'adminazure@servidormariadbtfg', 'emorcillos06@iesalbarregas.es');
+            return $dbh;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
         }
     }
+}
 ?>
